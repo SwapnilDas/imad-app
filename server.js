01bf920fae990ae09a37e8c7d738f06145db5191
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-10 04:22:57 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,59 +6,14 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
-    title:'ARTICLE ONE',
-    heading: 'ARTICLE ONE',
-    date: '8 Aug 2017',
-    content:`<p>This is my first article .... Do read..And don't forget to subscribe my channel
-           <div>
-               <p>The first article is all about my life.
-                  And what i wanna do in.
-               </p>`
-};
 
-function createTemplate(data)
-{
-    var heading=data.heading;
-    var title=data.title;
-    var date=data.date;
-    var content=data.content;
-    
-var htmlTemplate =`
-<html>
-<head>
-<title>${title}</title>
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-</head>
-<body>
-    <div class="container">
-        <div>
-            <a href ="/">Home
-            </a>
-        </div>
-             <h1>${heading}</h1>
-        <div>${date}</div>
-    
-        <div>
-        ${content}
-        </div>
-        </p>
-        </div>
-        <hr/>
-    </div>
-</body>
-</html>
-return htmlTemplate;
-
-}`}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res){
-      res.send(createTemplate(articleOne));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });
 

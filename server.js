a9eb72bec911981ab9f374dcd5cbe6dc614a0e77
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-11 05:03:10 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.send(createTemplate(article-one));
+    res.send(createTemplate(ArticleOne));
 });
 
 app.get('/article-two', function(req, res){

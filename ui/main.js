58fc22948b29289a55bcc5cbe8bf13162977860f
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-12 21:27:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,12 @@ console.log('Loaded!');
 
 //move the image
 var img=document.getElementById('madi');
+function moveRight(){
+    marginLeft =marginleft+10;
+    img.style.marginLeft=marginleft+'px';
+}
 img.onclick = function()
     {
+        var interval =setInterval(moveRight, 100);
         img.style.marginLeft='100px';
     };

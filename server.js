6060139b78afbc3c7e5c3f527ab203d61b775e2a
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-15 10:27:49 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,8 +36,7 @@ var htmlTemplate=
 <body>
     <div class="container">
         <div id="butn">
-<button type="button"><a href ="/">Home</a>Home</button>          
-<button type="button"><a href="article-one"></a></button>
+<button type="button"><a href ="/">Home</a></button>          
 <button type="button"><a href="article-one">Next</a></button>
 <button type="button"><a href="article-one">Last</a></button>
             

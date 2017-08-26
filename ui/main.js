83afc2c1810c9b9fe3ce735c79df43bbfc390418
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-25 20:22:20 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,12 +45,12 @@ img.onclick = function()
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclicke = function(){
+submit.onclick = function(){
     //make a request to the server and send name
     //capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3'];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for(var i=0; i<names.length; i++)
+    for(var i=0; i< names.length; i++)
         {
             list +='<li>' + names[i] + '</li>';
         }

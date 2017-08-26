73373cdb89cd7c0f5e0dd66c04104135f312aea8
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-25 20:16:08 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,7 +50,7 @@ submit.onclicke = function(){
     //capture a list of names and render it as a list
     var names = ['name1', 'name2', 'name3'];
     var list = '';
-    for(var i =0; i<names.length; i++)
+    for(var i=0; i<names.length; i++)
         {
             list +='<li>' + names[i] + '</li>';
         }

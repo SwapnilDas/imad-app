20cd12a0c4d72b3aa8bfea63e0551c5ff66fcfb2
SwapnilDas <30785876+SwapnilDas@users.noreply.github.com>
2017-08-25 19:43:51 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,33 +118,14 @@ app.get('/counter', function(req,res)
 });
 
 
-app.get('/articleName', function(req, res){
-    //articleName == article-one
-    //articles{articleName} == {} content object for article one
-    pool.query("SELECT * FROM article WHERE title ='" +req.params.articleName +"'", function(err, result){
-            if(err)
-                {
-                    res.status(500).send(err.toString());
-                }
-            
-            else {
-                if(result.rows.length ===0)
-                    {
-                        res.status(404).send('Article not Found');
-                    }
-                else 
-                    {
-                        var articleData = result.rows[0];
-                        res.send(createTemplate(articleName));
-                    }
-                }
-                
+
+
+
+
+app.get('/:articleName', function (req,res){
+   var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
-});
-
-
-
-
 
 
 
